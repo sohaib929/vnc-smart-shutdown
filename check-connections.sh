fb56020fee4a116b78b8f7d981488e98346e97f0
Selim B <506994+selim-b@users.noreply.github.com>
2020-11-22 22:44:18 +0300

    Update check-connections.sh

--- check-connections.sh
+++ check-connections.sh
@@ -1,40 +1,36 @@
-#!/bin/bash
+import json
+import boto3
 
-CONNECTIONS_PORT=5901
-LAST_ONLINE_FILE=/etc/vnc/.last_online
-SHUTDOWN_DURATION=1800
+def lambda_handler(event, context):
+    
+    region = event['queryStringParameters']['region']
+    instance_tag_value = event['queryStringParameters']['smartStartTag']
+    instance_ids = []
+    instance_ids_reboot = []
+    
+    ec2 = boto3.client('ec2', region_name=region)
 
-echo ""
-echo "#########################################"
-echo "Started Script at $(date +"%x %X %z")"
-echo "#########################################"
-echo ""
-
-CONNECTIONS=$(netstat -an | grep :$CONNECTIONS_PORT | grep ESTABLISHED | wc -l)
-echo "THERE IS CURRENTLY $CONNECTIONS OPEN CONNECTION(S)"
-
-if [ $CONNECTIONS == 0 ];
-then
-    NOW=$(date +"%s")
-    echo "NOW = $NOW"
-    LAST_ONLINE=$(stat -c %X $LAST_ONLINE_FILE)
-    echo "LAST ONLINE = $LAST_ONLINE"
-    DIFFERENCE=$(($NOW - $LAST_ONLINE))
-    echo "WAS ONLINE FROM $DIFFERENCE SECONDS"
-    if [ $DIFFERENCE -gt $SHUTDOWN_DURATION ]; 
-    then
-	echo " WILL SHUTDOWN in 1 minute"
-        /sbin/shutdown -h +1
-    else
-        echo "WILL NOT SHUTDOWN"
-    fi
-else
-    touch $LAST_ONLINE_FILE
-fi
-
-echo ""
-echo "#######################################"
-echo "Ended Script at $(date +"%x %X %z")"
-echo "#######################################"
-echo ""
-
+    filter = [{
+        'Name': 'tag:SmartStart', 
+        'Values': [instance_tag_value]
+    }]
+    
+    instances = ec2.describe_instances(Filters=filter)
+    for resevation in instances['Reservations']:
+        for reservation_instance in resevation['Instances']:
+            if reservation_instance['State']['Code'] == 80:
+                instance_ids.append(str(reservation_instance['InstanceId']))
+                
+            if reservation_instance['State']['Code'] == 16:
+                instance_ids_reboot.append(str(reservation_instance['InstanceId']))
+    
+    if len(instance_ids) > 0:
+        ec2.start_instances(InstanceIds=instance_ids)
+    
+    if len(instance_ids_reboot) > 0:    
+        ec2.reboot_instances(InstanceIds=instance_ids_reboot)
+        
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Started instance(s) {} and rebooted instance(s) {}'.format(instance_ids, instance_ids_reboot))
+    }
